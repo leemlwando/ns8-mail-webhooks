1d21c0734211bed4a91fc65deae9697d21f3a7f5
leemlwando <leemlwando@gmail.com>
2025-06-18 10:57:44 +0200

    Fix installation failure: Skip pulling non-existent backend image
    - Update 05pullimages to skip backend image pulling and set environment variable
    - Build backend image locally during configure phase in 85ensure_backend_image
    - Remove MongoDB image from org.nethserver.images as per IMPLEMENTATION.md (external MongoDB)
    - Ensure create-module returns success for installation

--- build-images.sh
+++ build-images.sh
@@ -39,7 +39,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=${repobase}/mail-webhooks-backend:${IMAGETAG:-latest}" \
+    --label="org.nethserver.images=ghcr.io/leemlwando/mail-webhooks-backend:latest" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
